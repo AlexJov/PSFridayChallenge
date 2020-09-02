618ac6081d7066979edf18df9bd62ccec04d9e91
alex_jov <aleksjovovic@gmail.com>
2020-09-02 20:34:07 +0100

    Add JavaApp files;

---
+++ JavaApplication/petApp/src/main/java/org/springframework/samples/petclinic/system/LambdaFunction.java
@@ -0,0 +1,57 @@
+package org.springframework.samples.petclinic.system;
+
+// snippet-start:[lambda.java1.invoke.import]
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.lambda.AWSLambda;
+import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.lambda.model.InvokeRequest;
+import com.amazonaws.services.lambda.model.InvokeResult;
+import com.amazonaws.services.lambda.model.ServiceException;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+
+import java.nio.charset.StandardCharsets;
+// snippet-end:[lambda.java1.invoke.import]
+
+public class LambdaFunction {
+
+    public static Integer Invoke(String functionName, String inputValue, Regions region, String accessKey, String accessKeySecret){
+
+            // snippet-start:[lambda.java1.invoke.main]
+
+            String payload = "{\n" +
+            " \"input\": \"" + inputValue + "\" \n" +
+            "}";
+
+            System.out.println(payload);
+
+            InvokeRequest invokeRequest = new InvokeRequest()
+                    .withFunctionName(functionName)
+                    .withPayload(payload);
+            InvokeResult invokeResult = null;
+
+            try {
+                BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, accessKeySecret);
+
+                AWSLambda awsLambda = AWSLambdaClientBuilder.standard()
+                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                        .withRegion(region).build();
+
+                invokeResult = awsLambda.invoke(invokeRequest);
+
+                String ans = new String(invokeResult.getPayload().array(), StandardCharsets.UTF_8);
+
+                //write out the return value
+                System.out.println(ans);
+
+            } catch (ServiceException e) {
+                System.out.println(e);
+            }
+
+            return invokeResult.getStatusCode();
+            // snippet-end:[lambda.java1.invoke.main]
+
+    }
+
+}
