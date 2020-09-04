96496c68bae5b135c4c508344d40f45f5cd6b1ae
alex_jov <aleksjovovic@gmail.com>
2020-09-04 10:35:18 +0100

    Change profile in lambda script to default;

--- AWSLambda/pets-fn/lambda-invoke-create-role-and-attach-policy.sh
+++ AWSLambda/pets-fn/lambda-invoke-create-role-and-attach-policy.sh
@@ -3,17 +3,17 @@
 # create role 
 aws iam create-role --role-name lambda-invoke-role \
 --assume-role-policy-document file://lambda-invoke-role.json \
---profile alex-appd
+--profile default
 
 # create policy
 aws iam create-policy --policy-name lambda-invoke-policy \
 --policy-document file://lambda-invoke-policy.json \
---profile alex-appd
+--profile default
 
 # attach policy to role
 aws iam attach-role-policy --policy-arn arn:aws:iam::ACCOUNT_ID_HERE:policy/lambda-invoke-policy \
 --role-name lambda-invoke-role \
---profile alex-appd
+--profile default
 
 exit 0
 
