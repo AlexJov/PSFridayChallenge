1c3595a1a9ca5147ac4411eb51be51b02b0fef33
Aleksandra Jovovic <aleksjovovic@gmail.com>
2020-10-26 15:37:50 +0000

    Update index.js
    typo

--- AWSLambda/products-fn/src/index.js
+++ AWSLambda/products-fn/src/index.js
@@ -2,7 +2,7 @@
 function getProducts() {
     var allProducts = [
         {"id" : 1, "name" : "Guinea pig grooming spray", "description" : "Keep your guinea pig looking fabulous.", "price" : 199},
-        {"id" : 2, "name" : "Parrot reward biscuits", "description" : "Even for the pickiest biscuit eaters find it rewarding.", "price" : 19},
+        {"id" : 2, "name" : "Parrot reward biscuits", "description" : "Even the pickiest biscuit eaters find it rewarding.", "price" : 19},
         {"id" : 3, "name" : "Fat-Cat diet supplement", "description" : "One per month per cat.", "price" : 2000},
         {"id" : 4, "name" : "Good Boi mini plush toy", "description" : "Irresistible to any puppy.", "price" : 28}
      ];
